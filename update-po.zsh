4330f0750445a3cb925782f0d412d8d7e93f4cc8
MrTweek <mrtweek@flupps.net>
2010-12-14 15:08:22 +0100

    adding update-po.zsh that automatically deploys translations from glotpress

---
+++ update-po.zsh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+
+
+for lang (de_DE ru_RU fi_FI pt_PT nl_NL) {
+    short=`echo -n $lang | perl -pe 's/^(..)_.+$/$1/g'`
+
+    echo "[$lang][$short]"
+    wget http://hitchwiki.org/translate/projects/maps/$short/$lang/export-translations -O /tmp/$short.po
+    msgfmt -o locale/$lang/LC_MESSAGES/maps.mo /tmp/$short.po
+    rm /tmp/$short.po
+
+}
+
