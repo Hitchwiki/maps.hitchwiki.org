4dc07cfb877b6a3d7552c86af30bcbcb7008db3c
MrTweek <mrtweek@flupps.net>
2010-12-16 17:18:16 +0100

    minor bugfix

--- update-po.zsh
+++ update-po.zsh
@@ -2,7 +2,7 @@
 
 for lang (de_DE ru_RU fi_FI pt_PT nl_NL zh_CN lt_LT) {
     short=`echo -n $lang | perl -pe 's/^(..)_.+$/$1/g'`
-    wget http://hitchwiki.org/translate/projects/maps/$short/$lang/export-translations -O /tmp/$short.po
+    wget -o /dev/null http://hitchwiki.org/translate/projects/maps/$short/$lang/export-translations -O /tmp/$short.po
     msgfmt -o locale/$lang/LC_MESSAGES/maps.mo /tmp/$short.po
     mv /tmp/$short.po locale/$lang/LC_MESSAGES/maps.po -f
 
