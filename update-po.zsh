dfe3593969669583514a487d7ab295f4997fc6a3
Mikael Korpela <simison@flupps.net>
2011-01-04 00:10:35 +0100

    french

--- update-po.zsh
+++ update-po.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-for lang (de_DE ru_RU ro_RO fi_FI pt_PT nl_NL zh_CN lt_LT pl_PL it_IT es_ES) {
+for lang (de_DE ru_RU ro_RO fi_FI pt_PT nl_NL zh_CN lt_LT pl_PL it_IT es_ES fr_FR) {
     short=`echo -n $lang | perl -pe 's/^(..)_.+$/$1/g'`
     wget -o /dev/null http://hitchwiki.org/translate/projects/maps/$short/$lang/export-translations -O /tmp/$short.po
     msgfmt -o locale/$lang/LC_MESSAGES/maps.mo /tmp/$short.po
