13fe05bb65215801e6628fcc78d1fd9de802f5df
K <k@guaka.org>
2025-10-22 15:24:30 +0100

    sync_upstream.py #36

--- cron.sh
+++ cron.sh
@@ -3,6 +3,8 @@
 */10 * * * * cd /app && /usr/bin/flock -n /tmp/fetch_nostr.lockfile bash -c '/usr/local/bin/flask --app hitch generate fetch_nostr' > logs/fetch_nostr.log 2>&1
 # every minute
 * * * * * cd /app && /usr/bin/flock -n /tmp/show.lockfile bash -c '/usr/local/bin/flask --app hitch generate show' > logs/show.log 2>&1
+# every day at 2 AM
+0 7 * * * cd /app && /usr/bin/flock -n /tmp/sync_upstream.lockfile bash -c '/usr/local/bin/flask --app hitch generate sync_upstream' > logs/sync_upstream.log 2>&1
 # each day at midnight
 # 0 0 * * * cd /app && /usr/bin/flock -n /tmp/dump.lockfile bash -c '. $HOME/.bashrc; /usr/local/bin/python flask --app hitch generate dump' > logs/dump.log 2>&1
 # every day at midnight
